1978d34ab35124324440d855a7abf00ec2f1a189
Mehmet Ozdinc <mehmetozdincc@gmail.com>
2023-07-29 21:56:13 +0300

    29. Temmuz ilerlemesi

--- website/views.py
+++ website/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 def home(request):
-    return render(request,'website/home.html',{})
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"Giriş Başarılı!")
+            return redirect('home')
+        else:
+            messages.error(request,"Bu işte bir terslik var!!!")
+            return redirect('home')
+    else:
+        return render(request,'website/home.html',{})
+
+
+
+def logout_user(request):
+    pass
